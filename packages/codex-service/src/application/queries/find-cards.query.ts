9c50a333ae68b213c1905cdaf0f9740529d82c81
Pavel Slotinsky <pslotinsky@gmail.com>
2025-06-08 18:20:19 +0300

    DOM: Make codex less strict again

--- packages/codex-service/src/application/queries/find-cards.query.ts
+++ packages/codex-service/src/application/queries/find-cards.query.ts
@@ -1,14 +1,12 @@
-import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { IQueryHandler, Query, QueryHandler } from '@nestjs/cqrs';
 
 import { Card } from '@service/domain/entities/card.entity';
 import { CardRepository } from '@service/domain/repositories/card.repository';
 
-export class FindCardsQuery {
-  constructor() {}
-}
+export class FindCardsQuery extends Query<Card[]> {}
 
 @QueryHandler(FindCardsQuery)
-export class FindCardsHandler implements IQueryHandler<FindCardsQuery, Card[]> {
+export class FindCardsHandler implements IQueryHandler<FindCardsQuery> {
   constructor(private readonly cardRepository: CardRepository) {}
 
   public async execute(): Promise<Card[]> {
--- packages/codex-service/src/application/queries/get-card.query.ts
+++ packages/codex-service/src/application/queries/get-card.query.ts
@@ -1,14 +1,16 @@
-import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { IQueryHandler, Query, QueryHandler } from '@nestjs/cqrs';
 
 import { Card } from '@service/domain/entities/card.entity';
 import { CardRepository } from '@service/domain/repositories/card.repository';
 
-export class GetCardQuery {
-  constructor(public readonly id: string) {}
+export class GetCardQuery extends Query<Card> {
+  constructor(public readonly id: string) {
+    super();
+  }
 }
 
 @QueryHandler(GetCardQuery)
-export class GetCardHandler implements IQueryHandler<GetCardQuery, Card> {
+export class GetCardHandler implements IQueryHandler<GetCardQuery> {
   constructor(private readonly cardRepository: CardRepository) {}
 
   public async execute({ id }: GetCardQuery): Promise<Card> {
