0bb91e876181e5293bf6e442398450adc9d0c5fe
Pavel Slotinsky <pslotinsky@gmail.com>
2025-06-22 02:06:12 +0300

    DOD-0005: Add tests for zok pli

---
+++ packages/zok/test/pli-base-cases.test.ts
@@ -0,0 +1,12 @@
+import test from "node:test";
+import assert from "node:assert";
+
+test.describe("Zok PLI. Base cases", () => {
+  test(`zok create task "Hello task"`, async () => {});
+
+  test(`zok list tasks`, async () => {});
+
+  test(`zok rename task DOD-0001 "Goodbye task"`, async () => {});
+
+  test(`zok close task DOD-0001`, async () => {});
+});
---
+++ packages/zok/test/pli-negative-cases.test.ts
@@ -0,0 +1,14 @@
+import test from "node:test";
+import assert from "node:assert";
+
+test.describe("Zok PLI. Base cases", () => {
+  test(`zok create table "Oops"`, async () => {});
+
+  test(`zok close task "DDD-0001"`, async () => {});
+
+  test(`zok sing task "DOD-0001"`, async () => {});
+
+  test(`zok rename task DOD-0001`, async () => {});
+
+  test(`zok create task`, async () => {});
+});
