7b102cff6c67da218cc0110582f36582771dd4e2
Pavel Slotinsky <pavel.slotinsky@acclaim.ai>
2026-04-14 02:51:13 +0300

    DOD-0009: Fix fonts & paddings for Council

--- apps/council-web/src/main.tsx
+++ apps/council-web/src/main.tsx
@@ -1,10 +1,3 @@
-import '@fontsource/cinzel/400.css';
-import '@fontsource/cinzel/600.css';
-import '@fontsource/cinzel/700.css';
-import '@fontsource/eb-garamond/400.css';
-import '@fontsource/eb-garamond/400-italic.css';
-import '@fontsource/eb-garamond/600.css';
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
